c6f9594503d0a05f6677d4149c3ca33d438de448
kevintor29 <112710345+kevintor29@users.noreply.github.com>
2022-09-28 15:33:51 -0300

    Add files via upload

---
+++ visualstudio/holamundo/falti/js5.js
@@ -0,0 +1,35 @@
+//Faça um programa que receba três inteiros e diga qual deles é o maior.
+function js51() {
+    let ent1=0, ent2=0,ent3=0,maior=0;
+    alert("programa que determina o maior de 3 numeros");
+    ent1=prompt("escreva ele numero 1");
+    ent2=prompt("escreva ele numero 2");
+    ent3=prompt("escreva ele numero 3");
+
+    if(ent1 >ent2){
+        maior=ent1;
+    } if(ent1 >ent3){
+        maior=ent1;
+    }if(ent2 >ent1){
+        maior=ent2;
+    }if(ent2 >ent3){
+        maior=ent2;
+    } if(ent3 >ent1){
+        maior=ent3;
+    } if(ent3 >ent2){
+        maior=ent3;
+    }
+  alert("o maior dos 3 numeros é : "+maior);
+}
+//Faça um programa que entre com três números e coloque em ordem crescente
+function fs52() {
+    let ent1=0, ent2=0,ent3=0,x=0;
+    alert("programa que determina o ordem crecente");
+    ent1=prompt("escreva ele numero 1");
+    ent2=prompt("escreva ele numero 2");
+    ent3=prompt("escreva ele numero 3");
+     
+    for(x=0;x=>2;x++){
+        
+    }
+}
